260895e250cbecaadfb4fd65ed5e4b0309d5c81b
fabiocat93 <fabio-cat93@hotmail.it>
2024-04-02 17:33:35 +0200

    drafting the general architecture

---
+++ src/pipepal/audio/tasks/__init__.py
@@ -0,0 +1,5 @@
+"""This module provides the implementation of pipepal tasks for audio."""
+
+from .example_task import Interface as ExampleTask
+
+__all__ = ['ExampleTask']
---
+++ src/pipepal/audio/tasks/example_task/services/example_service/service.py
@@ -0,0 +1,52 @@
+"""This module implements an example service for the task."""
+
+from typing import Any, Dict
+
+from pipepal.utils.abstract_service import AbstractService
+
+
+class Service(AbstractService):
+    """Example service that extends AbstractService."""
+
+    NAME: str = "ExampleService"
+
+    def __init__(self, configs: Dict[str, Any]) -> None: # noqa: ANN401
+        """Initialize the service with given configurations.
+
+        Args:
+            configs: A dictionary of configurations for the service.
+        """
+        super().__init__()
+
+    def preprocess(self, data: Any) -> Any: # noqa: ANN401
+        """Preprocess input data. Implementation can be customized.
+
+        Args:
+            data: The input data to preprocess.
+
+        Returns:
+            The preprocessed data.
+        """
+        return super().preprocess(data)
+
+    def process(self, data: Any) -> Dict[str, Any]: # noqa: ANN401
+        """Process input data. Custom implementation for ExampleService.
+
+        Args:
+            data: The input data to process.
+
+        Returns:
+            A dictionary containing 'output' key with a sample output.
+        """
+        return {"output": "ExampleService output"}
+
+    def postprocess(self, data: Any) -> Any: # noqa: ANN401
+        """Postprocess processed data. Implementation can be customized.
+
+        Args:
+            data: The data to postprocess.
+
+        Returns:
+            The postprocessed data.
+        """
+        return super().postprocess(data)
