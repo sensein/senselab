73506907e7f998520d25cdf25cf283d82cfe641d
fabiocat93 <fabio-cat93@hotmail.it>
2024-05-09 13:56:21 -0400

    adding parallelization

--- src/senselab/audio/tasks/speech_to_text.py
+++ src/senselab/audio/tasks/speech_to_text.py
@@ -1,22 +1,24 @@
 """This module implements some utilities for the speech-to-text task."""
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Enum, Union
 
 from datasets import Dataset
 from transformers import pipeline
+from pathlib import Path
 
-from senselab.utils.functions import _select_device_and_dtype
+from senselab.utils.functions import _select_device_and_dtype, DeviceType
 from senselab.utils.hf import HFModel
 from senselab.utils.tasks.input_output import _from_dict_to_hf_dataset, _from_hf_dataset_to_dict
 
-
 def transcribe_dataset_with_hf(dataset: Dict[str, Any], 
                                audio_column: str = 'audio',
-                               model_id: str = "openai/whisper-tiny", 
+                               model_id: Union[str, Path] = "openai/whisper-tiny", 
+                               model_revision: str = 'main',
                                language: Optional[str] = None,
                                return_timestamps: Optional[str] = 'word',
                                max_new_tokens: int = 128,
                                chunk_length_s: int = 30,
-                               batch_size: int = 16
+                               batch_size: int = 16,
+                               device: Optional[DeviceType] = None,
                                ) -> Dict[str, Any]:
     """Transcribes all audio samples in the dataset."""
     _ = HFModel(model_id=model_id) # check HF model is valid
@@ -24,20 +26,26 @@ def transcribe_dataset_with_hf(dataset: Dict[str, Any],
 
     def _prepare_hf_asr_pipeline(
         model_id: str,
-        language: Optional[str] = None,
-        return_timestamps: Optional[str] = 'word',
-        max_new_tokens: int = 128,
-        chunk_length_s: int = 30,
-        batch_size: int = 16
+        model_revision: str,
+        language: Optional[str],
+        return_timestamps: Optional[str],
+        max_new_tokens: int,
+        chunk_length_s: int,
+        batch_size: int,
+        device: Optional[DeviceType] = None
     ) -> pipeline:
         """Prepare a Hugging Face ASR pipeline."""
         _ = HFModel(model_id=model_id) # check HF model is valid
 
-        device, torch_dtype = _select_device_and_dtype(device_options=['cuda', 'cpu']) 
-        # TODO: dinamically check if 'mps' is supported by the model before excluding it a priori
+        if device is None:
+            device, torch_dtype = _select_device_and_dtype(device_options=[DeviceType.CUDA, DeviceType.CPU]) 
+            # MPS is not supported for now
+        else:
+            device, torch_dtype = _select_device_and_dtype(device_options=[device])
 
         pipe = pipeline("automatic-speech-recognition", 
                         model=model_id, 
+                        revision=model_revision,
                         return_timestamps=return_timestamps, 
                         max_new_tokens=max_new_tokens,
                         chunk_length_s=chunk_length_s,
@@ -47,6 +55,7 @@ def transcribe_dataset_with_hf(dataset: Dict[str, Any],
         return pipe
 
     pipe = _prepare_hf_asr_pipeline(model_id=model_id, 
+                                    model_revision=model_revision,
                                     language=language,
                                     return_timestamps=return_timestamps, 
                                     max_new_tokens=max_new_tokens,
@@ -61,7 +70,6 @@ def transcribe_dataset_with_hf(dataset: Dict[str, Any],
             transcriptions = pipe(batch[audio_column_name], generate_kwargs = {"language":f"<|{language}|>"})
         return {"asr": transcriptions}
 
-    transcript_dataset = hf_dataset.map(_transcribe_batch_with_hf_asr_pipeline, batched=True, batch_size=16, fn_kwargs={"pipe": pipe})
+    transcript_dataset = hf_dataset.map(_transcribe_batch_with_hf_asr_pipeline, batched=True, batch_size=batch_size, fn_kwargs={"pipe": pipe})
     transcript_dataset = transcript_dataset.remove_columns([audio_column])
-    print(transcript_dataset)
     return _from_hf_dataset_to_dict(transcript_dataset)
