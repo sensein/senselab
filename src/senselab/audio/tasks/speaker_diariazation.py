8037dc5dbb9b3e8c0a0097d6974c24ca02170208
ibevers <isaacbevers@gmail.com>
2024-05-14 15:31:37 -0400

    Add todo notes

--- src/senselab/audio/tasks/speaker_diariazation.py
+++ src/senselab/audio/tasks/speaker_diariazation.py
@@ -18,9 +18,9 @@ from datasets import load_dataset, load_dataset_builder, Audio
 from pyannote.audio.pipelines.utils.hook import ProgressHook
 from pyannote.audio import Pipeline
 
-access_token = "TOKEN_HERE"
+access_token = ""
 
-def pyannote_31_diarize(audio):
+def pyannote_31_diarize_row(audio):
     """
     Diarizes an audio file with Pyannote 3.1.
 
@@ -30,6 +30,7 @@ def pyannote_31_diarize(audio):
         "pyannote/speaker-diarization-3.1",
         use_auth_token=access_token)
 
+    #Possibly use Fabio's conversion code
     waveform = torch.tensor(audio['array'], dtype=torch.float32)
     if waveform.dim() == 1:
         waveform = waveform.unsqueeze(0)  # Add channel dimension
@@ -37,12 +38,13 @@ def pyannote_31_diarize(audio):
     if waveform.shape[0] > waveform.shape[1]:
         waveform = waveform.T
 
+    #Possibly remove
     with ProgressHook() as hook:
         diarization = pipeline({"waveform": waveform, "sample_rate": audio['sampling_rate']}, hook=hook)
 
     return diarization
 
-def diarize_hf_dataset(dataset):
+def pyannote_31_diarize_hf_dataset(dataset, batch=False, ):
     """
     Diarizes the audio files in a Hugging Face dataset.
     TODO: mapping and batching
@@ -50,12 +52,20 @@ def diarize_hf_dataset(dataset):
     """
     diarizations = []
     # dataset[i]['audio']
+    #torch batching here
+    #variable batch sizes
     for i in range(len(dataset['audio'])):
-        diarizations += [pyannote_31_diarize(dataset['audio'][i])] #loading audio twice
+        diarizations += [pyannote_31_diarize_row(dataset['audio'][i])] #loading audio twice
     return diarizations
 
 
 dataset = load_dataset("PolyAI/minds14", "en-US", split="train")
-diarizations = diarize_hf_dataset(dataset[:2])
+diarizations = pyannote_31_diarize_hf_dataset(dataset[:2])
 for diarization in diarizations:
     print(diarization)
+
+
+#apply row function to each row of dataset with map
+#create directory with atomic functions
+#merge  to get batching options
+#cuda
