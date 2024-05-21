8083df52fe77e2d2e71d4b955bc3a752f679ff3a
fabiocat93 <fabio-cat93@hotmail.it>
2024-05-21 12:26:12 -0400

    adding functioning example from huggingface

--- scripts/pyannote_31_experiment.py
+++ scripts/pyannote_31_experiment.py
@@ -5,6 +5,7 @@ from senselab.audio.tasks.preprocessing import resample_hf_dataset
 from senselab.utils.tasks.input_output import read_files_from_disk
 
 from senselab.audio.tasks.pyannote_31 import pyannote_31_diarize
+from senselab.utils.tasks.input_output import _from_hf_dataset_to_dict
 
 data = {"files": 
             ["../src/tests/data_for_testing/audio_48khz_mono_16bits.wav", 
@@ -25,3 +26,18 @@ print("Diarized dataset.")
 
 print(dataset_diarized)
 
+
+dataset = load_dataset("PolyAI/minds14", "en-US", split="train")
+dataset = dataset.select(range(2))
+
+dataset = _from_hf_dataset_to_dict(dataset)
+
+print("Resampling dataset...")
+dataset = resample_hf_dataset(dataset, 16000)
+print("Resampled dataset.")
+
+print("Diarizing dataset...")
+dataset_diarized = pyannote_31_diarize(dataset)
+print("Diarized dataset.")
+
+print(dataset_diarized)
