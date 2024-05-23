cc28bec15fe11c3f1963dba87a6dbb66a16aafb5
ibevers <isaacbevers@gmail.com>
2024-05-23 15:17:22 -0400

    Add batching and fix issue with all segments in return

--- src/senselab/audio/tasks/pyannote_31.py
+++ src/senselab/audio/tasks/pyannote_31.py
@@ -11,8 +11,6 @@ as an input an AudioFile object, that can be a map of "waveform" and "sample_rat
 hence it should be an easy adaptation of the code they have. you can look at the speech_to_text.py script to get inspired. 
 you can do mapping and batching to speed up the processio.py
 AudioFile = Union[Text, Path, IOBase, Mapping]
-
-TODO: batching
 """
 
 import torch
@@ -28,44 +26,51 @@ def annotation_to_dict(annotation):
     """
     Converts a pyannote.core.Annotation object to a dictionary.
     """
-    result = {}
+    result = []
     for segment, _, label in annotation.itertracks(yield_label=True):
-        result[str(segment)] = label
+        result.append((str(segment), label))
     return result
 
-def pyannote_31_diarize_row(row, hf_token):
+def pyannote_31_diarize_batch(batch,  hf_token):
     """
     Diarizes an audio file with Pyannote 3.1.
     :param audio: a dictionary containing 'array' and 'sampling_rate'
     """
-    audio = row['audio']
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
         use_auth_token=hf_token)
     
-    waveform = torch.tensor(audio['array'], dtype=torch.float32)
-    if waveform.dim() == 1:
-        waveform = waveform.unsqueeze(0)  # Add channel dimension
+    diarizations = []
+    for audio in batch['audio']:
+        waveform = torch.tensor(audio['array'], dtype=torch.float32)
 
-    if waveform.shape[0] > waveform.shape[1]:
-        waveform = waveform.T
+        if waveform.dim() == 1:
+            waveform = waveform.unsqueeze(0)  # Add channel dimension
 
-    with ProgressHook() as hook:
-        diarization = pipeline({"waveform": waveform, "sample_rate": audio['sampling_rate']}, hook=hook)
-    
-    diarization_dict = annotation_to_dict(diarization)
-    # print(diarization_dict)
-    return {"pyannote31_diarizations":diarization_dict}
+        if waveform.shape[0] > waveform.shape[1]:
+            waveform = waveform.T
 
-def pyannote_31_diarize(dataset: Dict[str, Any], hf_token=None):
+        with ProgressHook() as hook:
+            diarization = pipeline({"waveform": waveform, "sample_rate": batch['audio'][0]['sampling_rate']}, hook=hook)
+        
+        diarization = annotation_to_dict(diarization)
+        diarizations.append(diarization)
+    results = {'pyannote31_diarizations': diarizations}
+    return results    
+
+def pyannote_31_diarize(dataset: Dict[str, Any], 
+                        hf_token: str = None, 
+                        batched: bool = False, 
+                        batch_size: int = 1):
     """
     Diarizes the audio files in a Hugging Face dataset. The diarizations are added
     to the dataset as a new column 'pyannote31_diarization'
     """
     hf_dataset = _from_dict_to_hf_dataset(dataset)
-    result =  hf_dataset.map(lambda x: pyannote_31_diarize_row(x, hf_token))
-    result = result.remove_columns(['audio'])
+    result =  hf_dataset.map(lambda x: pyannote_31_diarize_batch(x, hf_token), 
+                             batched=batched, 
+                             batch_size=batch_size,
+                             cache_file_name="./cache",
+                             remove_columns=["audio"])
     result = _from_hf_dataset_to_dict(result)
-    result["pyannote31_diarizations"] = list(map(lambda d: {k: v for k, v in d.items() if v}, 
-                                            result["pyannote31_diarizations"]))
     return result
