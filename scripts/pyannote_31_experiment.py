2f8fe09b3de7427a4bc63ce15e39e2f36942c3e2
ibevers <isaacbevers@gmail.com>
2024-05-24 18:32:51 -0400

    Clean up and comment in Google style

--- scripts/pyannote_31_experiment.py
+++ scripts/pyannote_31_experiment.py
@@ -1,67 +1,22 @@
-import sys
+"""Demonstrates use of pyannote_31.py with the PolyAI/minds14 dataset.
+
+PolyAI/minds14 dataset:
+https://huggingface.co/datasets/PolyAI/minds14
+"""
+
+
 import json
 
-from datasets import load_dataset, load_dataset_builder, Audio
-
-
-
-# import sys
-# sys.path.append('/Users/isaacbevers/sensein/senselab/src/senselab')
-
-#/Users/isaacbevers/sensein/senselab/scripts
+from datasets import load_dataset
 
 from senselab.audio.tasks.preprocessing import resample_hf_dataset
-from senselab.utils.tasks.input_output import read_files_from_disk
-
-from senselab.audio.tasks.pyannote_31 import pyannote_31_diarize, pyannote_31_diarize_batch
+from senselab.audio.tasks.pyannote_31 import pyannote_31_diarize
 from senselab.utils.tasks.input_output import _from_hf_dataset_to_dict
-
-
-
-
-# data = {"files": 
-#             ["../src/tests/data_for_testing/audio_48khz_mono_16bits.wav", 
-#             "../src/tests/data_for_testing/audio_48khz_mono_16bits.wav" ]
-#         }
-
-
-# dataset = read_files_from_disk(data["files"])
-# print(f"Dataset loaded with {len(dataset)} records.")
-
-# print("Resampling dataset...")
-# dataset = resample_hf_dataset(dataset, 16000)
-# print("Resampled dataset.")
-
-# print("Diarizing dataset...")
-# dataset_diarized = pyannote_31_diarize(dataset)
-# print("Diarized dataset.")
-
-# print(dataset_diarized)
-
-
-# dataset = load_dataset("PolyAI/minds14", "en-US", split="train")
-# dataset = dataset.select(range(3))
-
-# dataset = _from_hf_dataset_to_dict(dataset)
-
-# print("Resampling dataset...")
-# dataset = resample_hf_dataset(dataset, 16000)
-# print("Resampled dataset.")
-
-# print("Diarizing dataset...")
-# dataset_diarized = pyannote_31_diarize(dataset)
-# print("Diarized dataset.")
-
-# print(json.dumps(dataset_diarized, indent=4))
 
 
 hf_token = "YOUR HF TOKEN"
 dataset = load_dataset("PolyAI/minds14", "en-US", split="train")
 dataset = dataset.select(range(4))
-# diarizations = []
-# for row in dataset:
-#     diarizations.append(pyannote_31_diarize_row(row, hf_token))
-# print(json.dumps(diarizations, indent=4))
 
 dataset = _from_hf_dataset_to_dict(dataset)
 
@@ -74,59 +29,3 @@ dataset_diarized = pyannote_31_diarize(dataset, batched=True, batch_size=2)
 print("Diarized dataset.")
 
 print(json.dumps(dataset_diarized, indent=4))
-
-
-
-#run 
-# Example usage:
-# dataset = load_dataset("your_dataset")
-# hf_token = "your_huggingface_api_token"
-# diarizations = pyannote_31_diarize(dataset, hf_token=hf_token)
-
-
-# def pyannote_31_diarize_row(audio, hf_token):
-#     """
-#     Diarizes an audio file with Pyannote 3.1.
-
-#     :p audio: a dictionary containing 'array' and 'sampling_rate'
-#     """
-#     pipeline = Pipeline.from_pretrained(
-#         "pyannote/speaker-diarization-3.1",
-#         use_auth_token=hf_token)
-
-#     #Possibly use Fabio's conversion code
-#     waveform = torch.tensor(audio['array'], dtype=torch.float32)
-#     if waveform.dim() == 1:
-#         waveform = waveform.unsqueeze(0)  # Add channel dimension
-
-#     if waveform.shape[0] > waveform.shape[1]:
-#         waveform = waveform.T
-
-#     #Possibly remove
-#     with ProgressHook() as hook:
-#         diarization = pipeline({"waveform": waveform, "sample_rate": audio['sampling_rate']}, hook=hook)
-
-#     return diarization
-
-# def pyannote_31_diarize(dataset, batch=False, hf_token=None):
-#     """
-#     Diarizes the audio files in a Hugging Face dataset.
-#     TODO: mapping and batching
-#     TODO: use Fabio's IO code
-#     """
-#     diarizations = []
-#     # dataset[i]['audio']
-#     #torch batching here
-#     #variable batch sizes
-#     diarizations = dataset.map(pyannote_31_diarize_row, dataset, hf_token=hf_token)
-#     # for i in range(len(dataset['audio'])):
-#     #     diarizations += [pyannote_31_diarize_row(dataset['audio'][i], hf_token)] #loading audio twice
-#     return diarizations
-
-
-# # diar = hf_dataset.map(lambda x: _resample_hf_row(x, resample_rate, rolloff))
-# #apply row function to each row of dataset with map
-# #create directory with atomic functions
-# #merge  to get batching options
-# #cuda
-# #Fabio's IO
--- src/senselab/audio/tasks/pyannote_31.py
+++ src/senselab/audio/tasks/pyannote_31.py
@@ -1,76 +1,111 @@
+"""Diarizes a dataset with Pyannote speaker diarization 3.1.
 
-"""
-Pyannote https://huggingface.co/pyannote/speaker-diarization-3.1
-
-function accepting as input a HFDataset and returning the output of 
-pyannote speaker-diarization model for each file. 
-
-pyannote-audio's pipeline accept 
-as an input an AudioFile object, that can be a map of "waveform" and "sample_rate" 
-(https://github.com/pyannote/pyannote-audio/blob/develop/pyannote/audio/core/io.py#L43), 
-hence it should be an easy adaptation of the code they have. you can look at the speech_to_text.py script to get inspired. 
-you can do mapping and batching to speed up the processio.py
-AudioFile = Union[Text, Path, IOBase, Mapping]
+Pyannote speaker diarization 3.1:
+https://huggingface.co/pyannote/speaker-diarization-3.1
 """
 
+
+import os
+from typing import Any, Dict, List, Tuple
+
+from datasets import Dataset
 import torch
-from typing import Any, Dict
-from datasets import load_dataset, load_dataset_builder, Audio
-
 from pyannote.audio import Pipeline
 from pyannote.audio.pipelines.utils.hook import ProgressHook
-from senselab.utils.tasks.input_output import _from_dict_to_hf_dataset, _from_hf_dataset_to_dict
+from pyannote.core import Annotation
+
+from senselab.utils.tasks.input_output import (
+    _from_dict_to_hf_dataset,
+    _from_hf_dataset_to_dict,
+)
 
 
-def annotation_to_dict(annotation):
+def annotation_to_dict(annotation: Annotation) -> List[Tuple]:
+    """Convert a Pyannote annotation to a list of tuples.
+
+    Args:
+        annotation: The Pyannote annotation object.
+
+    Returns:
+        dirization_list:
+          A list of tuples where each tuple contains a segment and a label.
     """
-    Converts a pyannote.core.Annotation object to a dictionary.
-    """
-    result = []
+    dirization_list = []
     for segment, _, label in annotation.itertracks(yield_label=True):
-        result.append((str(segment), label))
-    return result
+        dirization_list.append((str(segment), label))
+    return dirization_list
 
-def pyannote_31_diarize_batch(batch,  hf_token):
-    """
-    Diarizes an audio file with Pyannote 3.1.
-    :param audio: a dictionary containing 'array' and 'sampling_rate'
+
+def pyannote_31_diarize_batch(batch: Dataset, hf_token: str) -> Dict[str, Any]:
+    """Diarize a batch of audio files using the Pyannote diarization model.
+
+    Args:
+        batch: A batch of audio files from a Hugging Face dataset.
+        hf_token: The Hugging Face API token.
+
+    Returns:
+        A dictionary containing the diarizations for the batch.Becomes a 
+          column in the dataset when returned.
     """
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
         use_auth_token=hf_token)
-    
+
     diarizations = []
     for audio in batch['audio']:
         waveform = torch.tensor(audio['array'], dtype=torch.float32)
 
+        # Add the channel dimension if the waveform is 1-dimensional.
         if waveform.dim() == 1:
-            waveform = waveform.unsqueeze(0)  # Add channel dimension
+            waveform = waveform.unsqueeze(0)
 
+        # Transpose if the number of channels exceeds the number of samples.
         if waveform.shape[0] > waveform.shape[1]:
             waveform = waveform.T
 
+        # Apply the pipeline to the waveform.
         with ProgressHook() as hook:
-            diarization = pipeline({"waveform": waveform, "sample_rate": batch['audio'][0]['sampling_rate']}, hook=hook)
-        
+            diarization = pipeline(
+                {"waveform": waveform,
+                    "sample_rate": batch['audio'][0]['sampling_rate']},
+                hook=hook)
+
         diarization = annotation_to_dict(diarization)
         diarizations.append(diarization)
-    results = {'pyannote31_diarizations': diarizations}
-    return results    
+    return {'pyannote31_diarizations': diarizations}
 
-def pyannote_31_diarize(dataset: Dict[str, Any], 
-                        hf_token: str = None, 
-                        batched: bool = False, 
-                        batch_size: int = 1):
+
+def pyannote_31_diarize(dataset: Dict[str, Any],
+                        hf_token: str = None,
+                        batched: bool = False,
+                        batch_size: int = 1,
+                        cache_path: str = "scripts/cache/") -> Dict[str, Any]:
+    """Diarizes the audio files in a Hugging Face dataset.
+
+    The diarizations are
+    added to the dataset as a new column 'pyannote31_diarization'.
+
+    Args:
+        dataset: The dataset containing audio files to be diarized.
+        hf_token: The Hugging Face API token, if required for access.
+        batched: Whether to process the dataset in batches.
+        batch_size: Number of samples to process in each batch,
+          if batching is enabled.
+        cache_path: The path to the cache directory.
+
+    Returns:
+        hf_dataset_diarized: The dataset with an added column 
+          'pyannote31_diarization' containing the diarizations.
     """
-    Diarizes the audio files in a Hugging Face dataset. The diarizations are added
-    to the dataset as a new column 'pyannote31_diarization'
-    """
+    if not os.path.exists(cache_path):
+        os.mkdir(cache_path)
+
     hf_dataset = _from_dict_to_hf_dataset(dataset)
-    result =  hf_dataset.map(lambda x: pyannote_31_diarize_batch(x, hf_token), 
-                             batched=batched, 
-                             batch_size=batch_size,
-                             cache_file_name="./cache",
-                             remove_columns=["audio"])
-    result = _from_hf_dataset_to_dict(result)
-    return result
+    hf_dataset_diarized = hf_dataset.map(
+                            lambda x: pyannote_31_diarize_batch(x, hf_token),
+                            batched=batched,
+                            batch_size=batch_size,
+                            cache_file_name=cache_path + "pyannote31_cache",
+                            remove_columns=["audio"])
+    hf_dataset_diarized = _from_hf_dataset_to_dict(hf_dataset_diarized)
+    return hf_dataset_diarized
