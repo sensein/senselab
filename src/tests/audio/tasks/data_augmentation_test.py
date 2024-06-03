a74517e1572382f1f60761b8c72bd9ab19cec765
fabiocat93 <fabio-cat93@hotmail.it>
2024-06-03 18:47:42 -0400

    adding preprocessing functions

--- src/tests/audio/tasks/data_augmentation_test.py
+++ src/tests/audio/tasks/data_augmentation_test.py
@@ -20,16 +20,16 @@ def test_audio_data_augmentation() -> None:
     mono_inverted = augment_audio_dataset(mono_audio, apply_augmentation, batched=False)
     stereo_inverted = augment_audio_dataset(stereo_audio, apply_augmentation, batched=False)
     assert torch.equal(
-        mono_audio[0].audio_data, -1 * mono_inverted[0].audio_data
+        mono_audio[0].waveform, -1 * mono_inverted[0].waveform
     ), "Audio should have been inverted by the augmentation"
     assert torch.equal(
-        stereo_audio[0].audio_data, -1 * stereo_inverted[0].audio_data
+        stereo_audio[0].waveform, -1 * stereo_inverted[0].waveform
     ), "Audio should have been inverted by the augmentation and not affected by stereo audio"
 
     batched_audio = AudioDataset.generate_dataset_from_audio_data(
-        [stereo_audio[0].audio_data[0], stereo_audio[0].audio_data[1]], stereo_audio[0].sampling_rate
+        [stereo_audio[0].waveform[0], stereo_audio[0].waveform[1]], stereo_audio[0].sampling_rate
     ).create_split_for_pydra_task(True, 2)
     batch_inverted = augment_audio_dataset(batched_audio[0], apply_augmentation, batched=True)
-    assert torch.equal(batched_audio[0][0].audio_data, -1 * batch_inverted[0].audio_data) and torch.equal(
-        batched_audio[0][1].audio_data, -1 * batch_inverted[1].audio_data
+    assert torch.equal(batched_audio[0][0].waveform, -1 * batch_inverted[0].waveform) and torch.equal(
+        batched_audio[0][1].waveform, -1 * batch_inverted[1].waveform
     )
