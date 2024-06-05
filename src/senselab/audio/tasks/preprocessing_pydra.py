e95f8ec340fbf73e02779c0695f8d44d85b132b5
Jordan Wilke <wilke18@mit.edu>
2024-06-05 15:12:04 -0400

    Removed function calling delted function

--- src/senselab/audio/tasks/preprocessing_pydra.py
+++ src/senselab/audio/tasks/preprocessing_pydra.py
@@ -6,12 +6,10 @@ from senselab.audio.tasks.preprocessing import (
     chunk_audios,
     downmix_audios_to_mono,
     resample_audios,
-    resample_hf_dataset,
     select_channel_from_audios,
 )
 
 resample_audios_pt = pydra.mark.task(resample_audios)
 downmix_audios_to_mono_pt = pydra.mark.task(downmix_audios_to_mono)
 chunk_audios_pt = pydra.mark.task(chunk_audios)
-resample_hf_dataset_pt = pydra.mark.task(resample_hf_dataset)
 select_channel_from_audios_pt = pydra.mark.task(select_channel_from_audios)
