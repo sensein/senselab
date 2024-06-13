ebc6c017b3cc7e8e19815c234809b46d257c5691
ibevers <isaacbevers@gmail.com>
2024-06-13 18:16:19 -0400

    Initiate voice activity detection

---
+++ src/senselab/audio/tasks/voice_activity_detection/voice_activity_detection.py
@@ -0,0 +1,6 @@
+"""Detecting voice in long audio recordings.
+
+Tasks
+ Integrate all models for speaker diarization by pyannote.audio accessible through the Huggingface hub.
+ Include torchaudio methods for VAD.
+"""
