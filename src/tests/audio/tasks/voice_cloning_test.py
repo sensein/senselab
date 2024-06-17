59e0c998a6a6c2cfe6c0e99b99bb2ada1c2fd92e
fabiocat93 <fabio-cat93@hotmail.it>
2024-06-16 23:43:26 -0400

    skipping tests on github for voice cloning

--- src/tests/audio/tasks/voice_cloning_test.py
+++ src/tests/audio/tasks/voice_cloning_test.py
@@ -10,7 +10,7 @@ from senselab.audio.tasks.voice_cloning.api import clone_voices
 from senselab.utils.data_structures.device import DeviceType
 from senselab.utils.data_structures.model import TorchModel
 
-pytestmark = pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="Skipping all tests in this file")
+pytestmark = pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="Skipping all tests in this file")
 
 
 @pytest.fixture
@@ -20,10 +20,12 @@ def audio_sample() -> Audio:
     resampled_audios = resample_audios([mono_audio], 16000)
     return resampled_audios[0]
 
+
 @pytest.fixture
 def torch_model() -> TorchModel:
     """Fixture for torch model."""
     return TorchModel(path_or_uri="bshall/knn-vc", revision="master")
+
 
 def test_clone_voices_length_mismatch(audio_sample: Audio, torch_model: TorchModel) -> None:
     """Test length mismatch in source and target audios."""
@@ -31,12 +33,8 @@ def test_clone_voices_length_mismatch(audio_sample: Audio, torch_model: TorchMod
     target_audios = [audio_sample, audio_sample]
 
     with pytest.raises(ValueError, match="Source and target audios must have the same length."):
-        clone_voices(
-            source_audios=source_audios,
-            target_audios=target_audios,
-            model=torch_model,
-            device=DeviceType.CPU
-        )
+        clone_voices(source_audios=source_audios, target_audios=target_audios, model=torch_model, device=DeviceType.CPU)
+
 
 def test_clone_voices_invalid_topk(audio_sample: Audio, torch_model: TorchModel) -> None:
     """Test invalid topk value."""
@@ -49,8 +47,9 @@ def test_clone_voices_invalid_topk(audio_sample: Audio, torch_model: TorchModel)
             target_audios=target_audios,
             model=torch_model,
             device=DeviceType.CPU,
-            topk="invalid" # type: ignore[arg-type]
+            topk="invalid",  # type: ignore[arg-type]
         )
+
 
 def test_clone_voices_invalid_prematched_vocoder(audio_sample: Audio, torch_model: TorchModel) -> None:
     """Test invalid prematched_vocoder value."""
@@ -63,8 +62,9 @@ def test_clone_voices_invalid_prematched_vocoder(audio_sample: Audio, torch_mode
             target_audios=target_audios,
             model=torch_model,
             device=DeviceType.CPU,
-            prematched_vocoder="invalid" # type: ignore[arg-type]
+            prematched_vocoder="invalid",  # type: ignore[arg-type]
         )
+
 
 def test_clone_voices_unsupported_model(audio_sample: Audio) -> None:
     """Test unsupported model."""
@@ -74,13 +74,11 @@ def test_clone_voices_unsupported_model(audio_sample: Audio) -> None:
 
     with pytest.raises(NotImplementedError, match="Only KNNVC is supported for now."):
         clone_voices(
-            source_audios=source_audios,
-            target_audios=target_audios,
-            model=unsupported_model,
-            device=DeviceType.CPU
+            source_audios=source_audios, target_audios=target_audios, model=unsupported_model, device=DeviceType.CPU
         )
 
-@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="Not enough space for this test on GitHub")
+
+@pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="Not enough space for this test on GitHub")
 def test_clone_voices_valid_input(audio_sample: Audio, torch_model: TorchModel) -> None:
     """Test cloning voices with valid input."""
     source_audios = [audio_sample]
@@ -93,7 +91,7 @@ def test_clone_voices_valid_input(audio_sample: Audio, torch_model: TorchModel) 
             model=torch_model,
             device=DeviceType.CPU,
             topk=5,
-            prematched_vocoder=False
+            prematched_vocoder=False,
         )
         assert isinstance(cloned_output, list), "Output must be a list."
         assert len(cloned_output) == 1, "Output list should contain exactly one audio sample."
@@ -106,7 +104,7 @@ def test_clone_voices_valid_input(audio_sample: Audio, torch_model: TorchModel) 
 
         # Check if the absolute difference is within the tolerance
         assert abs(source_duration - cloned_duration) <= tolerance, (
-        f"Cloned audio duration is not within acceptable range. Source: {source_duration}, "
+            f"Cloned audio duration is not within acceptable range. Source: {source_duration}, "
             f"Cloned: {cloned_duration}"
         )
 
