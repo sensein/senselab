ccbbdcf51eee01d8da23c8713fee754d492e6e97
ibevers <isaacbevers@gmail.com>
2024-07-10 20:44:47 -0400

    Add checks for model training sample rate.

--- src/senselab/audio/tasks/speaker_verification/speaker_verification.py
+++ src/senselab/audio/tasks/speaker_verification/speaker_verification.py
@@ -5,7 +5,7 @@ verifying if two audio samples or files are from the same speaker using a
 specified model.
 """
 
-import typing as ty
+from typing import Tuple
 
 from speechbrain.inference.speaker import SpeakerRecognition
 
@@ -18,21 +18,30 @@ def verify_speaker(
     audio1: Audio,
     audio2: Audio,
     model: SpeechBrainModel = SpeechBrainModel(path_or_uri="speechbrain/spkrec-ecapa-voxceleb", revision="main"),
+    model_training_sample_rate: int = 16000,  # spkrec-ecapa-voxceleb trained on 16kHz audio
     device: DeviceType = DeviceType.CPU,
-) -> ty.Tuple[float, bool]:
+) -> Tuple[float, bool]:
     """Verifies if two audio samples are from the same speaker.
 
     Args:
         audio1 (Audio): The first audio sample.
         audio2 (Audio): The second audio sample.
-        model (SpeechBrainModel): The model for speaker verification.
-                                  Defaults to the ECAPA-TDNN model.
-        device (DeviceType): The device to run the model on. Defaults to CPU.
+        model (SpeechBrainModel, optional): The model for speaker verification.
+        model_training_sample_rate (int, optional): The sample rate the model trained on.
+        device (DeviceType, optional): The device to run the model on. Defaults to CPU.
 
     Returns:
-        Tuple[float, bool]: The verification score and prediction.
-                            The score is a float, and the prediction is a boolean.
+        Tuple[float, bool]: A tuple containing the verification score and the prediction.
+                            The verification score is a float indicating the similarity
+                            between the two samples, and the prediction is a boolean
+                            indicating if the two samples are from the same speaker.
     """
+    if audio1.sampling_rate != model_training_sample_rate:
+        raise ValueError(f"{model.path_or_uri} trained on {model_training_sample_rate} \
+                            sample audio, but audio1 has sample rate {audio1.sampling_rate}.")
+    if audio2.sampling_rate != model_training_sample_rate:
+        raise ValueError(f"{model.path_or_uri} trained on {model_training_sample_rate} \
+                         sample audio, but audio1 has sample rate {audio2.sampling_rate}.")
     verification = SpeakerRecognition.from_hparams(source=model.path_or_uri, run_opts={"device": device.value})
     score, prediction = verification.verify_batch(audio1.waveform, audio2.waveform)
     return float(score), bool(prediction)
