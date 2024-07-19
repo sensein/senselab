56f39c3e5117a16fe5fb2c670f9dfb226c84e6d6
Jordan Wilke <wilke18@mit.edu>
2024-07-19 10:57:20 -0500

    Add tensor serializer

---
+++ src/senselab/utils/data_structures/pydra_helpers.py
@@ -0,0 +1,19 @@
+"""Functionality for Pydra."""
+
+from typing import Iterator
+
+import numpy as np
+import torch
+from pydra.utils.hash import Cache, bytes_repr_sequence_contents, register_serializer
+
+
+@register_serializer(torch.Tensor)
+def bytes_repr_arraylike(obj: torch.Tensor, cache: Cache) -> Iterator[bytes]:
+    """Register a serializer for Torch tensors that allows Pydra to properly use them."""
+    yield f"{obj.__class__.__module__}{obj.__class__.__name__}:".encode()
+    array = np.asanyarray(obj)
+    yield f"{array.size}:".encode()
+    if array.dtype == "object":
+        yield from bytes_repr_sequence_contents(iter(array.ravel()), cache)
+    else:
+        yield array.tobytes(order="C")
