2e5a50609275b5e3aa85419ff98b76858b9eb985
fabiocat93 <fabio-cat93@hotmail.it>
2024-07-23 15:06:28 -0400

    testing speech to text with multiple models

--- src/tests/audio/tasks/speech_to_text_test.py
+++ src/tests/audio/tasks/speech_to_text_test.py
@@ -11,8 +11,7 @@ def test_scriptline_from_dict() -> None:
     """Test creating ScriptLine from dict."""
     data = {
         "text": "Hello world",
-        "chunks": [{"text": "Hello", "timestamps": [0.0, 1.0]},
-                   {"text": "world", "timestamps": [1.0, 2.0]}],
+        "chunks": [{"text": "Hello", "timestamps": [0.0, 1.0]}, {"text": "world", "timestamps": [1.0, 2.0]}],
     }
     scriptline = ScriptLine.from_dict(data)
 
@@ -41,13 +40,17 @@ if os.getenv("GITHUB_ACTIONS") != "true":
         """Fixture for Hugging Face model."""
         return HFModel(path_or_uri="openai/whisper-tiny")
 
+    @pytest.fixture
+    def hf_model2() -> HFModel:
+        """Fixture for Hugging Face model."""
+        return HFModel(path_or_uri="facebook/seamless-m4t-unity-small")
 
+    @pytest.mark.parametrize("hf_model", ["hf_model", "hf_model2"], indirect=True)
     def test_transcribe_audios(mono_audio_sample: Audio, hf_model: HFModel) -> None:
         """Test transcribing audios."""
         transcripts = transcribe_audios(audios=[mono_audio_sample], model=hf_model)
         assert len(transcripts) == 1
         assert isinstance(transcripts[0], ScriptLine)
-
 
     def test_transcribe_audios_with_transformers(mono_audio_sample: Audio, hf_model: HFModel) -> None:
         """Test transcribing audios with transformers."""
@@ -63,7 +66,6 @@ if os.getenv("GITHUB_ACTIONS") != "true":
         )
         assert len(transcripts) == 1
         assert isinstance(transcripts[0], ScriptLine)
-
 
     def test_asr_pipeline_factory(hf_model: HFModel) -> None:
         """Test ASR pipeline factory."""
@@ -85,9 +87,7 @@ if os.getenv("GITHUB_ACTIONS") != "true":
         )
         assert pipeline1 is pipeline2  # Check if the same instance is returned
 
-
-    def test_transcribe_stereo_audio(resampled_stereo_audio_sample: Audio, 
-                                     hf_model: HFModel) -> None:
+    def test_transcribe_stereo_audio(resampled_stereo_audio_sample: Audio, hf_model: HFModel) -> None:
         """Test transcribing stereo audio."""
         # Create a mock stereo audio sample
         with pytest.raises(ValueError, match="Stereo audio is not supported"):
