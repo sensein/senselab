1029d042707d1f4b491ed4d4676449c063ec326b
Jordan Wilke <wilke18@mit.edu>
2024-07-23 15:53:32 -0400

    Pydra helpers for tensors

--- src/senselab/utils/data_structures/pydra_helpers.py
+++ src/senselab/utils/data_structures/pydra_helpers.py
@@ -17,3 +17,32 @@ def bytes_repr_arraylike(obj: torch.Tensor, cache: Cache) -> Iterator[bytes]:
         yield from bytes_repr_sequence_contents(iter(array.ravel()), cache)
     else:
         yield array.tobytes(order="C")
+
+
+# Pydra runner
+# need function that allows for marking a task (could be obfuscated internally)
+# need function that runs the actual code/does the parallelization
+# getting results is a bit clunky right now so could clean that up
+# internal Pydra wouldn't need obfuscations for add/split but external API might be useful
+# perhaps dictionary structure could be intuitive? linked list?
+
+# what if we had a simple split/run structure:
+# user: run X task with Y audios (Satra example); perhaps extraneous information of DeviceType, Model to use
+#   - kinda easy example of just create the pydra setup ourselves, no user worries
+# Senselab "pipelines"/Tasks:
+
+# example:
+# RAVDESS audios:
+# - need to resample
+# - perhaps run loudness normalization
+# - run SER
+# - run evaluation
+
+# get RAVDESS audios
+# split into batches
+#   - run batches through resmapling
+#   - pass to normalization
+#   - run SER
+#   - get partial evaluation
+# - combine evaluation
+# - give average
