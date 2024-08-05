df73b648e92537fa86e3fdfdb387b85ea91b77ac
ibevers <isaacbevers@gmail.com>
2024-08-05 17:05:51 -0400

    Update redundant imports

--- src/senselab/audio/tasks/data_augmentation/__init__.py
+++ src/senselab/audio/tasks/data_augmentation/__init__.py
@@ -1,1 +1,6 @@
 """This module provides the API of the senselab audio data augmentation."""
+from senselab.audio.tasks.data_augmentation.data_augmentation import augment_audios
+
+__all__ = [
+    "augment_audios",
+]
--- src/senselab/audio/tasks/forced_alignment/__init__.py
+++ src/senselab/audio/tasks/forced_alignment/__init__.py
@@ -1,1 +1,6 @@
 """This module provides the API for forced alignment."""
+from senselab.audio.tasks.forced_alignment.forced_alignment import align_transcriptions
+
+__all__ = [
+    "align_transcriptions",
+]
