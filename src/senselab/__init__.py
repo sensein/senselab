2a3d2fc7a52bde922e2b956e0d245e37cd18f335
Satrajit Ghosh <satra@mit.edu>
2024-11-13 00:07:23 -0500

    add nest asyncio to package list

--- src/senselab/__init__.py
+++ src/senselab/__init__.py
@@ -1,7 +1,7 @@
 """.. include:: ../../README.md"""  # noqa: D415
 
 import nest_asyncio
+nest_asyncio.apply()
 
 from senselab.utils.data_structures.pydra_helpers import *  # NOQA
 
-nest_asyncio.apply()
