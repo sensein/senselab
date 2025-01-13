fc20beb801fa7dcebb0ac95f67a54299640b0c91
brukew <brukew@mit.edu>
2025-01-13 16:03:27 -0500

    Update pose estimation init

--- src/senselab/video/tasks/pose_estimation/__init__.py
+++ src/senselab/video/tasks/pose_estimation/__init__.py
@@ -1,1 +1,8 @@
 """This module defines the API for pose estimation."""
+
+from senselab.video.tasks.pose_estimation.api import estimate_pose, visualize_pose  # noqa: F401
+from senselab.video.tasks.pose_estimation.estimate import (  # noqa: F401
+    MediaPipePoseEstimator,
+    YOLOPoseEstimator,
+)
+from senselab.video.tasks.pose_estimation.visualization import visualize  # noqa: F401
