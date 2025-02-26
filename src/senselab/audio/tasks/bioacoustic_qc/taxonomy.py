28f2efb5545557df33b1048a0f7663856f44c8ed
ibevers <isaacbevers@gmail.com>
2025-02-26 15:05:52 -0500

    Add bioacoustic_task taxonomy

---
+++ src/senselab/audio/tasks/bioacoustic_qc/taxonomy.py
@@ -0,0 +1,29 @@
+"""Taxonomy of bioacoustic tasks emphasizing human tasks."""
+
+BIOACOUSTIC_TASK_TAXONOMY = {
+    "bioacoustic_task": {
+        "human": {
+            "respiration": {
+                "breathing": {
+                    "quiet": None,
+                    "deep": None,
+                    "rapid": None,
+                    "sigh": None,
+                },
+                "exhalation": {
+                    "cough": {"voluntary": None, "reflexive": None},
+                },
+                "inhalation": {"sniff": None, "gasp": None},
+            },
+            "vocalization": {
+                "speech": {
+                    "spontaneous_speech": None,
+                    "read_speech": None,
+                    "repetitive_speech": {"diadochokinesis": None, "counting": None},
+                    "sustained_phonation": None,
+                },
+                "non_speech": {"laughter": None, "crying": None, "humming": None, "throat_clearing": None},
+            },
+        }
+    }
+}
