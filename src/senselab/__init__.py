2fbfb3be8a737fe05f2bea3bab6f98aecfd60fdf
fabiocat93 <fabio-cat93@hotmail.it>
2025-02-26 17:17:58 -0500

    fixing style - ignoring ANN101 and ANN102

--- src/senselab/__init__.py
+++ src/senselab/__init__.py
@@ -5,7 +5,7 @@ from multiprocessing import set_start_method
 
 if platform.system() == "Darwin" and platform.machine() != "arm64":
     raise RuntimeError(
-        "Error: This package requires an ARM64 architecture on macOS " \
+        "Error: This package requires an ARM64 architecture on macOS "
         "since PyTorch 2.2.2+ does not support x86-64 on macOS."
     )
 
