ae088815301f903f2631aec1053efa47d36c0520
ibevers <isaacbevers@gmail.com>
2025-02-27 10:43:12 -0500

    Make bioacoustic_qc.py functions importable

--- src/senselab/audio/tasks/bioacoustic_qc/__init__.py
+++ src/senselab/audio/tasks/bioacoustic_qc/__init__.py
@@ -1,1 +1,17 @@
 """Provides the API for bioacoustic quality control."""
+
+from .bioacoustic_qc import (
+    audios_to_task_dict,
+    check_quality,
+    task_dict_to_dataset_taxonomy_subtree,
+    tasks_to_taxonomy_tree_path,
+    taxonomy_subtree_to_pydra_workflow,
+)
+
+__all__ = [
+    "audios_to_task_dict",
+    "check_quality",
+    "task_dict_to_dataset_taxonomy_subtree",
+    "tasks_to_taxonomy_tree_path",
+    "taxonomy_subtree_to_pydra_workflow",
+]
