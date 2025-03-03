92e97f91493cf0209b9da587cb7205163fcb501e
ibevers <isaacbevers@gmail.com>
2025-03-02 19:36:15 -0500

    Update check to iterate over task_audios

--- src/senselab/audio/tasks/bioacoustic_qc/checks.py
+++ src/senselab/audio/tasks/bioacoustic_qc/checks.py
@@ -13,60 +13,62 @@ All checks take a list of Audio objects as input and return:
     - A list of Audio objects that passed the check.
 """
 
-from typing import Dict, List, Tuple
+from typing import Dict, List
 
 import torch
 
 from senselab.audio.data_structures import Audio
 
 
-def audio_length_positive_check(audios: List[Audio]) -> Tuple[Dict[str, List[Audio]], List[Audio]]:
+def audio_length_positive_check(audios: List[Audio], task_audios: List[Audio]) -> Dict[str, List[Audio]]:
     """Checks if an Audio object has a positive length.
 
     Args:
         audios (Audio): The Audio object to validate.
+        task_audios (List[Audio]): The subset of Audio objects to check.
 
     Returns:
-        Tuple[Dict[str, List[Audio] | None], List[Audio]]: A tuple containing:
+        Dict[str, List[Audio]]: A tuple containing:
             - A dictionary with:
                 - "exclude": A list of Audio objects that failed the check.
                 - "review": Always None (not used in this check).
-            - A list of Audio objects that passed the check.
+                - "passed": List of Audio objects that passed the check.
     """
     exclude = []
     passed = []
 
-    for audio in audios:
+    for audio in task_audios:  # iterate over task_audios
         if audio.waveform.numel() == 0:  # No samples
             exclude.append(audio)
-            audios.remove(audio)
+            audios.remove(audio)  # remove from all audios
         else:
             passed.append(audio)
 
-    return {"exclude": exclude, "review": []}, passed
+    return {"exclude": exclude, "review": [], "passed": passed}
 
 
-def audio_intensity_positive_check(audios: List[Audio]) -> Tuple[Dict[str, List[Audio]], List[Audio]]:
-    """Checks if an Audio object has nonzero intensity.
+def audio_intensity_positive_check(audios: List[Audio], task_audios: List[Audio]) -> Dict[str, List[Audio]]:
+    """Checks if each Audio object has nonzero intensity.
 
     Args:
-        audios (Audio): The Audio object to validate.
+        audios (List[Audio]): The complete dataset of Audio objects.
+        task_audios (List[Audio]): The subset of Audio objects to check.
 
     Returns:
-        Tuple[Dict[str, List[Audio] | None], List[Audio]]: A tuple containing:
+        Dict[str, List[Audio]]: A tuple containing:
             - A dictionary with:
-                - "exclude": A list of Audio objects that failed the check.
-                - "review": Always None (not used in this check).
-            - A list of Audio objects that pasgised the check.
+                - "exclude": List of Audio objects that have zero intensity.
+                - "review": Always an empty list (not used in this check).
+                - "passed": List of Audio objects that passed the check.
     """
     exclude = []
     passed = []
 
-    for audio in audios:
+    for audio in task_audios:
         if torch.sum(torch.abs(audio.waveform)) == 0:
             exclude.append(audio)
             audios.remove(audio)
         else:
             passed.append(audio)
 
-    return {"exclude": exclude, "review": []}, passed
+    return {"exclude": exclude, "review": [], "passed": passed}
