2031f355198a3715250c232e288010e47c6ca1dc
ibevers <isaacbevers@gmail.com>
2025-03-04 10:21:48 -0500

    Remove redundant check_quality in __init__.py

--- src/senselab/audio/tasks/bioacoustic_qc/__init__.py
+++ src/senselab/audio/tasks/bioacoustic_qc/__init__.py
@@ -12,7 +12,6 @@ from .bioacoustic_qc import (
 
 __all__ = [
     "audios_to_task_dict",
-    "check_quality",
     "task_dict_to_dataset_taxonomy_subtree",
     "task_to_taxonomy_tree_path",
     "taxonomy_subtree_to_pydra_workflow",
