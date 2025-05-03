d7903c86d67416ddeb910d2c0b7290cb1d70d1d0
brukew <brukew@mit.edu>
2025-05-03 15:49:16 -0400

    fix spelling

--- src/senselab/video/__init__.py
+++ src/senselab/video/__init__.py
@@ -3,7 +3,7 @@
 try:
     from deepface import DeepFace
 
-    # This is to prevent deepface from braking tensorflow
+    # This is to prevent deepface from breaking tensorflow
     # (see https://github.com/serengil/deepface/issues/1390:
     # Importing tensorflow before deepface breaks it)
 except ImportError:
