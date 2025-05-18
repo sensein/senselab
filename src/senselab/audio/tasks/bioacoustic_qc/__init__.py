974c7f566bac838ad52c3d57a9ab925de54f7469
ibevers <isaacbevers@gmail.com>
2025-05-18 14:05:59 -0400

    Add  subtree_to_evaluations to init

--- src/senselab/audio/tasks/bioacoustic_qc/__init__.py
+++ src/senselab/audio/tasks/bioacoustic_qc/__init__.py
@@ -1,5 +1,13 @@
 """Provides the API for bioacoustic quality control."""
 
-from .bioacoustic_qc import activity_to_dataset_taxonomy_subtree, check_quality
+from .bioacoustic_qc import (
+    subtree_to_evaluations,
+    activity_to_dataset_taxonomy_subtree, 
+    check_quality
+)
 
-__all__ = ["activity_to_dataset_taxonomy_subtree", "check_quality"]
+__all__ = [
+    "subtree_to_evaluations",
+    "activity_to_dataset_taxonomy_subtree",
+    "check_quality"
+]
