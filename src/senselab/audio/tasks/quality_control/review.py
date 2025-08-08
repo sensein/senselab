5d865919a5373c7967e0ed7e12baabe8372ce9a7
ibevers <isaacbevers@gmail.com>
2025-08-08 14:15:43 -0400

    Start quality control review module

---
+++ src/senselab/audio/tasks/quality_control/review.py
@@ -0,0 +1,9 @@
+"""Uses weak supervision to label files as include, exclude, or unsure."""
+
+
+# load the evaluation results
+# get the check columns
+# define label functions that map True to exclude, False to unsure
+# train the label model
+# apply the label model to the data
+# save the results
