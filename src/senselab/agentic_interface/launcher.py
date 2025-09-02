e57186d0f96eed35db4b5207f4ffaf0021d11b50
fabiocat93 <fabio-cat93@hotmail.it>
2025-09-02 11:13:09 +0200

    fixing git sync issues

--- src/senselab/agentic_interface/launcher.py
+++ src/senselab/agentic_interface/launcher.py
@@ -7,8 +7,6 @@ from .senselab_agent.install import ensure_installed
 
 def senselab_ai():
     # Install the example AI Agent extension
-    print("Ensuring AI Agent Example extension is installed...")
     ensure_installed()
     # pass through args to jupyter
-    print("Launching Jupyter Lab...")
     sys.exit(subprocess.call(["jupyter", "lab", *sys.argv[1:]]))
