3e1b6c8919bf1f155018b2054c9cccc5ee7c7433
rfbrito <rfbrito@node1703.inband>
2025-09-03 14:28:56 -0400

    comment out  batch processing diarized objects and file name parsing from primary speaker ratio metric

--- src/senselab/audio/tasks/quality_control/metrics.py
+++ src/senselab/audio/tasks/quality_control/metrics.py
@@ -525,58 +525,56 @@ def primary_speaker_ratio_metric(audio: Audio, **input_source) -> float:
 
     Returns:
         float: Ratio of primary speaker's duration to total duration.
+                If there are no speakers, return nan
+                If only one speaker, return 1.0
+                Else return a number between 0.0 and 1.0
     """
 
     if input_source['precompute'] is None:
-        diar #= #diarize the uadio
+        diar #= #TODO diarize the audio
     else:
         diar = input_source['precompute']
+        #TODO diar = pd.read_pickle("../../modeling/diarize/diar_r2.pkl")
     
-    
-    
-    def primary_speaker_ratio(diar_obj):
-        try:
-            ratio = diar_obj.label_duration(diar_obj.argmax()) / np.sum([c[1] for c in diar_obj.chart()])
-        except:
-            ratio = np.nan
+    # Calculate primary speaker ratio;
+    try:
+        ratio = diar_obj.label_duration(diar_obj.argmax()) / np.sum([c[1] for c in diar_obj.chart()])
+    except:
+        ratio = np.nan
 
-        return ratio
 
-    ##should work; how to make this part fit into existing architecuture
-    #diar = pd.read_pickle("../../modeling/diarize/diar_r2.pkl")
+    # sub = []
+    # ses = []
+    # task = []
+    # speaker_count = []
+    # diar_obj = {}
+    # main_speaker_ratio = []
 
-    sub = []
-    ses = []
-    task = []
-    speaker_count = []
-    diar_obj = {}
-    main_speaker_ratio = []
-
-    for k, r in diar.items():
+    #for k, r in diar.items():
         
-        #probably not needed 
-        sb = Path(k).stem.split("sub-")[1].split("_ses")[0]
-        ss = Path(k).stem.split("ses-")[1].split("_task")[0]
-        tk = Path(k).stem.split("task-")[1].split(".wav")[0]
+        # #probably not needed 
+        # sb = Path(k).stem.split("sub-")[1].split("_ses")[0]
+        # ss = Path(k).stem.split("ses-")[1].split("_task")[0]
+        # tk = Path(k).stem.split("task-")[1].split(".wav")[0]
 
-        sub.append(sb)
-        ses.append(ss)
-        task.append(tk)
+        # sub.append(sb)
+        # ses.append(ss)
+        # task.append(tk)
         # silent.append(r.get_timeline().duration()< 0.5)
         # diar_result.append(r.get_timeline().duration())
-        speaker_count.append(len(r.labels()))
-        diar_obj[(sb, ss, tk)] = r
-        main_speaker_ratio.append(primary_speaker_ratio(r))
+        # speaker_count.append(len(r.labels()))
+        # diar_obj[(sb, ss, tk)] = r
+        #main_speaker_ratio.append(primary_speaker_ratio(r))
 
-    diar_speaker_check = pd.DataFrame(
-        {
-            "record_id": sub,
-            "session_id": ses,
-            "task": task,
-            "speaker_count": speaker_count,
-            "primary_speaker_ratio": main_speaker_ratio,
-        }
-    )
-    diar_obj_filtered = {tuple(f): diar_obj[tuple(f)] for f in features[["record_id", "session_id", "task"]].values}
+    #diar_speaker_check = pd.DataFrame(
+    #     {
+    #         "record_id": sub,
+    #         "session_id": ses,
+    #         "task": task,
+    #         "speaker_count": speaker_count,
+    #         "primary_speaker_ratio": main_speaker_ratio,
+    #     }
+    # )
+    #diar_obj_filtered = {tuple(f): diar_obj[tuple(f)] for f in features[["record_id", "session_id", "task"]].values}
 
-    return 0.75  # Example fixed value; replace with actual computation
+    return ratio
