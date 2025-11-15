78df14931912641ee5ba8bdd7941b100befca8ac
ibevers <isaacbevers@gmail.com>
2025-11-15 18:57:02 -0500

    Remove unused metrics and checks

--- src/senselab/audio/tasks/quality_control/taxonomies/bioacoustic.py
+++ src/senselab/audio/tasks/quality_control/taxonomies/bioacoustic.py
@@ -1,30 +1,18 @@
 """Bioacoustic activity taxonomy tree implementation."""
 
 from senselab.audio.tasks.quality_control.checks import (
-    audio_intensity_positive_check,
-    audio_length_positive_check,
+    completely_silent_check,
+    high_proportion_clipped_check,
+    low_spectral_gating_snr_check,
+    very_low_amplitude_modulation_depth_check,
+    very_low_peak_snr_from_spectral_check,
 )
 from senselab.audio.tasks.quality_control.metrics import (
-    amplitude_headroom_metric,
-    amplitude_interquartile_range_metric,
-    amplitude_kurtosis_metric,
     amplitude_modulation_depth_metric,
-    amplitude_skew_metric,
-    crest_factor_metric,
-    dynamic_range_metric,
-    mean_absolute_amplitude_metric,
-    mean_absolute_deviation_metric,
     peak_snr_from_spectral_metric,
-    phase_correlation_metric,
     proportion_clipped_metric,
-    proportion_silence_at_beginning_metric,
-    proportion_silence_at_end_metric,
     proportion_silent_metric,
-    root_mean_square_energy_metric,
-    shannon_entropy_amplitude_metric,
-    signal_variance_metric,
     spectral_gating_snr_metric,
-    zero_crossing_rate_metric,
 )
 from senselab.audio.tasks.quality_control.taxonomy import TaxonomyNode
 
@@ -39,30 +27,18 @@ def build_bioacoustic_activity_taxonomy() -> TaxonomyNode:
     root = TaxonomyNode(
         name="bioacoustic",
         checks=[
-            audio_length_positive_check,
-            audio_intensity_positive_check,
+            high_proportion_clipped_check,
+            completely_silent_check,
+            very_low_peak_snr_from_spectral_check,
+            low_spectral_gating_snr_check,
+            very_low_amplitude_modulation_depth_check,
         ],
         metrics=[
-            proportion_silent_metric,
-            proportion_silence_at_beginning_metric,
-            proportion_silence_at_end_metric,
-            amplitude_headroom_metric,
-            spectral_gating_snr_metric,
             proportion_clipped_metric,
-            amplitude_modulation_depth_metric,
-            root_mean_square_energy_metric,
-            zero_crossing_rate_metric,
-            signal_variance_metric,
-            dynamic_range_metric,
-            mean_absolute_amplitude_metric,
-            mean_absolute_deviation_metric,
-            shannon_entropy_amplitude_metric,
-            crest_factor_metric,
+            proportion_silent_metric,
             peak_snr_from_spectral_metric,
-            amplitude_skew_metric,
-            amplitude_kurtosis_metric,
-            amplitude_interquartile_range_metric,
-            phase_correlation_metric,
+            spectral_gating_snr_metric,
+            amplitude_modulation_depth_metric,
         ],
     )
 
@@ -83,14 +59,26 @@ def build_bioacoustic_activity_taxonomy() -> TaxonomyNode:
     exhalation.add_child("cough", cough)
 
     # Cough types for Bridge2AI - specific file mappings
-    audio_check_3 = TaxonomyNode(name="audio_check_3", checks=[], metrics=[])
-    respiration_and_cough_cough_1 = TaxonomyNode(name="respiration_and_cough_cough_1", checks=[], metrics=[])
-    respiration_and_cough_cough_2 = TaxonomyNode(name="respiration_and_cough_cough_2", checks=[], metrics=[])
-    voluntary_cough = TaxonomyNode(name="voluntary_cough", checks=[], metrics=[])
+    audio_check_3 = TaxonomyNode(
+        name="audio_check_3", checks=[], metrics=[]
+    )
+    respiration_and_cough_cough_1 = TaxonomyNode(
+        name="respiration_and_cough_cough_1", checks=[], metrics=[]
+    )
+    respiration_and_cough_cough_2 = TaxonomyNode(
+        name="respiration_and_cough_cough_2", checks=[], metrics=[]
+    )
+    voluntary_cough = TaxonomyNode(
+        name="voluntary_cough", checks=[], metrics=[]
+    )
 
     cough.add_child("audio_check_3", audio_check_3)
-    cough.add_child("respiration_and_cough_cough_1", respiration_and_cough_cough_1)
-    cough.add_child("respiration_and_cough_cough_2", respiration_and_cough_cough_2)
+    cough.add_child(
+        "respiration_and_cough_cough_1", respiration_and_cough_cough_1
+    )
+    cough.add_child(
+        "respiration_and_cough_cough_2", respiration_and_cough_cough_2
+    )
     cough.add_child("voluntary_cough", voluntary_cough)
 
     # Create vocalization branch
@@ -116,7 +104,9 @@ def build_bioacoustic_activity_taxonomy() -> TaxonomyNode:
     laughter = TaxonomyNode(name="laughter", checks=[], metrics=[])
     crying = TaxonomyNode(name="crying", checks=[], metrics=[])
     humming = TaxonomyNode(name="humming", checks=[], metrics=[])
-    throat_clearing = TaxonomyNode(name="throat_clearing", checks=[], metrics=[])
+    throat_clearing = TaxonomyNode(
+        name="throat_clearing", checks=[], metrics=[]
+    )
 
     non_speech.add_child("laughter", laughter)
     non_speech.add_child("crying", crying)
@@ -124,23 +114,49 @@ def build_bioacoustic_activity_taxonomy() -> TaxonomyNode:
     non_speech.add_child("throat_clearing", throat_clearing)
 
     # Productive vocabulary under unscripted
-    productive_vocabulary = TaxonomyNode(name="productive_vocabulary", checks=[], metrics=[])
+    productive_vocabulary = TaxonomyNode(
+        name="productive_vocabulary", checks=[], metrics=[]
+    )
     unscripted.add_child("productive_vocabulary", productive_vocabulary)
 
     # Specific productive vocabulary files
-    productive_vocabulary_1 = TaxonomyNode(name="productive_vocabulary_1", checks=[], metrics=[])
-    productive_vocabulary_2 = TaxonomyNode(name="productive_vocabulary_2", checks=[], metrics=[])
-    productive_vocabulary_3 = TaxonomyNode(name="productive_vocabulary_3", checks=[], metrics=[])
-    productive_vocabulary_4 = TaxonomyNode(name="productive_vocabulary_4", checks=[], metrics=[])
-    productive_vocabulary_5 = TaxonomyNode(name="productive_vocabulary_5", checks=[], metrics=[])
-    productive_vocabulary_6 = TaxonomyNode(name="productive_vocabulary_6", checks=[], metrics=[])
+    productive_vocabulary_1 = TaxonomyNode(
+        name="productive_vocabulary_1", checks=[], metrics=[]
+    )
+    productive_vocabulary_2 = TaxonomyNode(
+        name="productive_vocabulary_2", checks=[], metrics=[]
+    )
+    productive_vocabulary_3 = TaxonomyNode(
+        name="productive_vocabulary_3", checks=[], metrics=[]
+    )
+    productive_vocabulary_4 = TaxonomyNode(
+        name="productive_vocabulary_4", checks=[], metrics=[]
+    )
+    productive_vocabulary_5 = TaxonomyNode(
+        name="productive_vocabulary_5", checks=[], metrics=[]
+    )
+    productive_vocabulary_6 = TaxonomyNode(
+        name="productive_vocabulary_6", checks=[], metrics=[]
+    )
 
-    productive_vocabulary.add_child("productive_vocabulary_1", productive_vocabulary_1)
-    productive_vocabulary.add_child("productive_vocabulary_2", productive_vocabulary_2)
-    productive_vocabulary.add_child("productive_vocabulary_3", productive_vocabulary_3)
-    productive_vocabulary.add_child("productive_vocabulary_4", productive_vocabulary_4)
-    productive_vocabulary.add_child("productive_vocabulary_5", productive_vocabulary_5)
-    productive_vocabulary.add_child("productive_vocabulary_6", productive_vocabulary_6)
+    productive_vocabulary.add_child(
+        "productive_vocabulary_1", productive_vocabulary_1
+    )
+    productive_vocabulary.add_child(
+        "productive_vocabulary_2", productive_vocabulary_2
+    )
+    productive_vocabulary.add_child(
+        "productive_vocabulary_3", productive_vocabulary_3
+    )
+    productive_vocabulary.add_child(
+        "productive_vocabulary_4", productive_vocabulary_4
+    )
+    productive_vocabulary.add_child(
+        "productive_vocabulary_5", productive_vocabulary_5
+    )
+    productive_vocabulary.add_child(
+        "productive_vocabulary_6", productive_vocabulary_6
+    )
 
     return root
 
